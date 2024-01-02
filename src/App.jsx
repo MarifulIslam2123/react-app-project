8aae900b312a967d77573bdafaaec1e0a4398ccb
MarifulIslam2123 <remudhaka@gmail.com>
2024-01-03 00:48:39 +0600

    Add daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
   return (
     <>
       <h2 className='text-center text-3xl text-cyan-300 bg-gray-950'> hello React</h2>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }

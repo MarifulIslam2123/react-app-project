e3fbfe836e23b7ff194764b58478ea33272c65b5
MarifulIslam2123 <remudhaka@gmail.com>
2024-01-03 00:09:30 +0600

    New Change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h2 className='text-center text-3xl text-cyan-300 bg-gray-950'> hell remu</h2>
+      <h2 className='text-center text-3xl text-cyan-300 bg-gray-950'> hello React</h2>
     </>
   )
 }
